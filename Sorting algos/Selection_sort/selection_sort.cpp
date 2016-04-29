f04e86a39fe2ed8fbe32ac29f2cad8f162b36f43
creativenikhil <creativenikhil20@gmail.com>
2016-04-29 18:48:24 +0530

    Selection_sort added

---
+++ Sorting algos/Selection_sort/selection_sort.cpp
@@ -0,0 +1,37 @@
+//
+// Created by nikhi on २९-०४-२०१६.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T> void selection_sort(T arr[],int n){
+    T temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int int_arr[] = {6,5,4,3,2};
+    float float_arr[] = {6.43,5.343,7.232,3.454,1.567,0.565};
+    selection_sort(int_arr,5);
+    selection_sort(float_arr,6);
+
+    cout<<"Sorted array is :"<<endl;
+    for(int i=0;i<6;i++){
+        cout<<float_arr[i]<<" ";
+    }
+
+}
