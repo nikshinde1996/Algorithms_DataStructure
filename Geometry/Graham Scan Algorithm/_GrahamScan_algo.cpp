0ac8e743f3b7a7c825ea8b407cfa1798aad77d97
creativenikhil <creativenikhil20@gmail.com>
2016-07-28 23:58:37 +0530

    Graham Scan algorithm for convex hull

--- Geometry/Graham Scan Algorithm/_GrahamScan_algo.cpp
+++ Geometry/Graham Scan Algorithm/_GrahamScan_algo.cpp
@@ -1,2 +1,22 @@
 
 #include "_GrahamScan_algo.h"
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+
+struct Point{
+    int x,y;
+};
+
+void convexHull(Point p[],int n);
+int dis_square(Point p1,Point p2);
+int orientation(Point p1,Point p2,Point p3);
+void swap(Point &p1,Point &p2);
+Point next_to_top(stack<Point> s);
+int compare();
+
+int main(){
+    Point p[] = {{0,3},{1,1},{2,2},{4,4},{0,0},{1,2},{3,1},{3,3}};
+    int n = sizeof(p)/ sizeof(p[0]);
+    convexHull(p,n);
+}
