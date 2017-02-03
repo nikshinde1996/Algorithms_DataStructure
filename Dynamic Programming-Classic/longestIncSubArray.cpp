d9723fa506935de5a75fc08cf4ab3c54b15ee355
nikshinde1996 <crenickshinde1996@gmail.com>
2017-02-03 22:49:40 +0530

    old dp added

--- Dynamic Programming-Classic/longestIncSubArray.cpp
+++ Dynamic Programming-Classic/longestIncSubArray.cpp
@@ -3,14 +3,23 @@
 using namespace std;
 
 int longestSubSequence(int *arr,int n){
-    int len = 0;
-    for(int i=0;i<n;i++){
-      cout<<arr[i]
-    }
-    return len;
+  int len = 0;
+  int dp[n];
+  for(int i=0;i<n;i++)dp[i]=1;
+
+  for(int i=1 ;i<n;i++){
+     if(arr[i]>arr[i-1]){
+       dp[i] = max(dp[i-1]+1,dp[i]);
+     }else{
+       dp[i] = 1;
+     }
+  }
+  for(int i=0;i<n;i++) cout<<dp[i]<<" ";
+  cout<<endl;
+  return *std::max_element(dp,dp+n);
 }
 
 int main(){
-    int arr[10] = {5, 21, 10, 13, 50, 34,87};
+    int arr[] = {5, 21, 10, 13, 50, 34,87};
     cout<<longestSubSequence(arr,sizeof(arr)/sizeof(arr[0]));
 }
